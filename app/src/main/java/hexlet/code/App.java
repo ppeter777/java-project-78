0bb050e68623da7135704802158e103c1dca8fdc
Peter Pesotskiy <ppeter777@yandex.ru>
2023-10-10 23:38:19 +0300

    add App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,28 @@
 package hexlet.code;
 
 public class App {
+    public static void main(String[] args) {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+        System.out.println(schema.isValid(""));
+        System.out.println(schema.isValid(null));
+        System.out.println("");
+        schema.required();
+
+        schema.required();
+        System.out.println(schema.isValid(""));
+        System.out.println(schema.isValid(null));
+        System.out.println(schema.isValid(5));
+        System.out.println(schema.isValid("what does the fox say"));
+        System.out.println(schema.isValid("hexlet"));
+        System.out.println("");
+
+        System.out.println(schema.contains("wh").isValid("what does the fox say"));
+        System.out.println(schema.contains("what").isValid("what does the fox say"));
+        System.out.println(schema.contains("whatthe").isValid("what does the fox say"));
+        System.out.println(schema.isValid("what does the fox say"));
+
+//        StringSchema schema = v.string().required().minLength(4).contains("dfg");
+//        System.out.println(schema.isValid("dfgi"));
+    }
 }
