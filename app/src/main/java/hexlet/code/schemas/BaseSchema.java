02d9d9a73b9fc04dd425e2c0ac4148ed063db918
Peter Pesotskiy <ppeter777@yandex.ru>
2024-02-13 14:45:48 +0300

    add type parameter to BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-public abstract class BaseSchema {
+public abstract class BaseSchema<S> {
     private List<Predicate> checks = new ArrayList<>();
     private boolean isRequired;
 
